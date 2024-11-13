95bd941f16ce183c3c5061048ab942b6368bc066
Etore Marcari Junior <emarcari@gmail.com>
2024-11-13 08:30:24 -0300

    Meu primeiro commit

---
+++ main.py
@@ -0,0 +1,15 @@
+print("Jornada de Dados")
+
+idade = 47
+nome = "Etore"
+
+print(idade)
+
+idade2 = idade + 1
+print(idade2)
+
+lista = [1,2,3,4,5]
+print(lista)
+
+lista_invertida = lista.reverse()
+print(lista_invertida)
